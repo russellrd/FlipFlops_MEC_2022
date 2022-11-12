235370b6b25a78071cdef6a82f8ded0fc1d0e3f5
ganeshad <ganeshad@mcmaster.ca>
2022-11-12 10:46:14 -0500

    impl gyro

--- sensor.py
+++ sensor.py
@@ -2,4 +2,17 @@ class Accelerometer:
     pass
 
 class Gyroscope :
-    pass
+	angle = 0
+	name = ""
+ 
+    def __init__(self, name):
+    	self.name = 0
+
+    def set_angle(self, angle):
+    	pass
+
+    def get_angle(self):
+    	return angle
+
+    def __str__(self):
+    	return "Name: " + self.name + " Angle: " + self.angle
