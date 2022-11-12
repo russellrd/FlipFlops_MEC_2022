b3fa9f76359d4971d44ab54ae0d2ede6456067e3
ganeshad <ganeshad@mcmaster.ca>
2022-11-12 16:06:37 -0500

    fixed

--- gyroscope.py
+++ gyroscope.py
@@ -42,6 +42,7 @@ class Gyroscope :
 		if (abs(self.prev_angle - data.get_dir()) > self.config["MAX_ANGLE"]):
 			self.status = Status.Error
 			self.turn_off()
+			self.status = Status.Error
 		else:
 			if (self.power):
 				self.angle = data.get_dir();
