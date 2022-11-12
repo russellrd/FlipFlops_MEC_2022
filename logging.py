09eadf841c03075e7cda88436d3f140eb5e20053
ganeshad <ganeshad@mcmaster.ca>
2022-11-12 12:52:04 -0500

    fix

--- logging.py
+++ logging.py
@@ -2,6 +2,7 @@ class loggine():
 	file_name = ""
 	fh = None
 	levelDict = {"P": "Power", "E": "Error", "GA": "Angle"}
+	
 	def __init__(self, file_name):
 		self.file_name = file_name
 		self.fh = open(self.file_name, "w")
@@ -12,4 +13,3 @@ class loggine():
 	def log(self, level, data):
 		info = self.levelDict[level] + " " + data + "\n"
 		self.fh.write(info)
-
