b3c4d8a66e68993a8e8d0465df7a047134a2c7bc
ganeshad <ganeshad@mcmaster.ca>
2022-11-12 14:41:08 -0500

    added

--- log.py
+++ log.py
@@ -2,7 +2,8 @@ class Log():
 	file_name = ""
 	fh = None
 	levelDict = {"P": "Power", "E": "Error", "GA": "Angle", 
-	"A-X": "X-Speed", "A-Y":"Y-Speed", "A-M" :" Mag", "A-V" : "Velocity"}
+	"A-X": "X-Speed", "A-Y":"Y-Speed", "A-M" :" Mag", "A-V" : "Velocity"
+	"A-A": "Acceleration"}
 
 	def __init__(self, file_name):
 		self.file_name = file_name
