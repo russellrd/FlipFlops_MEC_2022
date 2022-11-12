42b996c841b1e0d8d0ba68889ca00176be035c81
ganeshad <ganeshad@mcmaster.ca>
2022-11-12 12:55:09 -0500

    fix

--- log.py
+++ log.py
@@ -2,11 +2,7 @@ class log():
 	file_name = ""
 	fh = None
 	levelDict = {"P": "Power", "E": "Error", "GA": "Angle"}
-<<<<<<< HEAD:logging.py
-	
-=======
 
->>>>>>> bb1adb0e359c80f6baaf5d91317ce23df6e8cb2a:log.py
 	def __init__(self, file_name):
 		self.file_name = file_name
 		self.fh = open(self.file_name, "w")
