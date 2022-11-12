746da63f10a74ae0dae07299e8a7706b37d34667
KresAl <alshehak@mcmaster.ca>
2022-11-12 14:30:14 -0500

    6th push

--- accelerometer.py
+++ accelerometer.py
@@ -1,6 +1,5 @@
 import math
-
-
+import status
 
 class Accelerometer: 
     AccX= 0 
@@ -9,17 +8,28 @@ class Accelerometer:
     time = 0 
     magn = 0
     velocity = 0 
+    power = False
+    state = None
 
-    def __init__(self, name):
-        self.name = 0
-	
+    config = None
+
+    def __init__(self, name, logger, config):
+        self.name = name
+        self.logger = logger
+        self.status = Status()
+        self.state = self.status.Idle.name
+        self.config = config
+
     def set_x(self, AccX):
+        self.logger.log("A-X", AccX)
         self.AccX
 
     def set_y (self, AccY):
+        self.logger.log("A-Y", AccY)
         self.AccY
 
     def set_speed (self, speed): 
+        self.logger.log("A-S", speed)
         self.speed
 
     def set_time (self, time): 
@@ -29,26 +39,27 @@ class Accelerometer:
         self.magn
 
     def set_velocity (self, velocity):
+        self.logger.log("A-V", velocity)
         self.velocity
 
     def get_x (self):
-        return AccX
-    
+        return self.AccX
+
     def get_y (self):
-        return AccY
+        return self.AccY
     
     def get_speed(self):
         self.calc_mag()
-        return speed
+        return self.speed
     
     def get_time(self): 
-        return time
+        return self.time
     
     def get_magn(self): 
-        return magn
+        return self.magn
     
     def get_velocity(self):
-        return velocity 
+        return self.velocity 
     
     def calc_mag(self):
 
@@ -58,6 +69,16 @@ class Accelerometer:
         self.calc_mag ()
         self.velocity = self.speed / self.time
 
+    def update(self, data):
+        return {"power" : self.power, "Status" : self.state, "velocity" : self.velocity}
+
+    def turn_on(self):
+        self.state = self.status.Running.name
+        self.power = True
+
+    def turn_off(self):
+        self.state = self.status.Idle.name
+        self.power = False
+
     def __str__(self):
         return "Name: " + self.name + " Speed: " + self.speed
-    
