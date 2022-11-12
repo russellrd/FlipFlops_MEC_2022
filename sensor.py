a562ed4f4e4fcb4ef9fe5cd3c67d8996ab1c1db8
ganeshad <ganeshad@mcmaster.ca>
2022-11-12 12:22:56 -0500

    fixed

--- sensor.py
+++ sensor.py
@@ -1,32 +1,30 @@
-class Accelerometer:
-    pass
-
 class Gyroscope :
 	angle = 0
 	name = ""
 	status = 1
 	power = 1
- 
-    def __init__(self, name):
-    	self.name = 0
+ 	def __init__(self, name,logger):
+		self.name = 0
+		self.logger = logger
 
-    def set_angle(self, angle):
-    	self.angle = angle
+	def set_angle(self, angle):
+		logger.log("GA", angle)
+		self.angle = angle
 
-    def get_angle(self):
-    	return angle
+	def get_angle(self):
+		return angle
 
-    def get_status(self):
-    	return status
+	def get_status(self):
+		return status
 
-    def turn_on(self):
-    	self.power = 1
+	def turn_on(self):
+		self.power = 1
 
-    def turn_off(self):
-    	self.power = 0
+	def turn_off(self):
+		self.power = 0
 
-  	def get_update(self):
-  		return {"power" : self.power, "status:" : self.status, "angle" : self.angle}
+		def update(self):
+			return {"power" : self.power, "status:" : self.status, "angle" : self.angle}
 
-    def __str__(self):
-    	return "Name: " + self.name + " Angle: " + self.angle
+	def __str__(self):
+		return "Name: " + self.name + " Angle: " + self.angle
