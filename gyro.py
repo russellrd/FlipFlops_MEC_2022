71931d9a46b79ee26bf00bc77f8e67fc66d97ba3
ganeshad <ganeshad@mcmaster.ca>
2022-11-12 12:24:06 -0500

    renamed

---
+++ gyro.py
@@ -0,0 +1,30 @@
+class Gyroscope :
+	angle = 0
+	name = ""
+	status = 1
+	power = 1
+ 	def __init__(self, name,logger):
+		self.name = 0
+		self.logger = logger
+
+	def set_angle(self, angle):
+		logger.log("GA", angle)
+		self.angle = angle
+
+	def get_angle(self):
+		return angle
+
+	def get_status(self):
+		return status
+
+	def turn_on(self):
+		self.power = 1
+
+	def turn_off(self):
+		self.power = 0
+
+		def update(self):
+			return {"power" : self.power, "status:" : self.status, "angle" : self.angle}
+
+	def __str__(self):
+		return "Name: " + self.name + " Angle: " + self.angle
